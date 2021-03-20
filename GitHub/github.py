3c13ddb248156be25ded09e43e15e12b2df1b07e
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-03-20 11:29:41 +0100

    Initial commit

---
+++ GitHub/github.py
@@ -0,0 +1,10 @@
+# This script will be run every hour for an entire week
+
+# List of all locations from where we'll ping (+-10 per continent)
+
+# Do tests from each location
+# Ping GitHub
+
+# If first time today, perform a traceroute
+
+# Store the found data for future analysis
