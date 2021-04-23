2eeda57dcea234e3589a1eff901189ebf9bf3336
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-04-23 23:28:18 +0200

    Automatically create measurement graph

--- Brexit/measurement_probe_filter.py
+++ Brexit/measurement_probe_filter.py
@@ -1,5 +1,6 @@
 from util import *
 from datetime import datetime
+from ping_measurement_analyzer import make_maesurement_graph
 
 # Geen handige manier gevonden om measurements per country te krijgen. Ik ga itereren over de ASN's en deze telkens als target ASN instellen.
 
@@ -9,12 +10,14 @@ probes = data["data"]["probes"]
 count = 0
 i = 0
 for probe in probes:
-    start_min = datetime(2020, 1, 14, 0, 0).timestamp()
-    start_max = datetime(2021, 1, 31, 0, 0).timestamp()
-    end_min = datetime(2021, 2, 1, 0, 0).timestamp()
-    end_max = datetime(2021, 2, 15, 0, 0).timestamp()
+    start_min = datetime(2020, 11, 1, 0, 0).timestamp()
+    start_max = datetime(2021, 1, 14, 0, 0).timestamp()
+    end_min = datetime(2021, 2, 14, 0, 0).timestamp()
+    end_max = datetime(2021, 4, 1, 0, 0).timestamp()
 
-    data = get_data("https://atlas.ripe.net/api/v2/measurements/ping/?", current_probes=probe['id'], start_time__gt=start_min, start_time__lt=start_max, end_time__gt=end_min, end_time__lt=end_max, is_all_scheduled=True, interval__gte=60*60, interval__lte=60*60*24*7)
+    data = get_data("https://atlas.ripe.net/api/v2/measurements/ping/?", current_probes=probe['id'], start_time__gt=start_min, start_time__lt=start_max, stop_time__gt=end_min, stop_time__lt=end_max)
+    # data = get_data("https://atlas.ripe.net/api/v2/measurements/ping/?", current_probes=probe['id'],
+    #                 start_time__lt=start_max, stop_time__gt=end_min)
 
     if i % 10:
         print(f"{i}/{len(probes)}")
@@ -22,7 +25,9 @@ for probe in probes:
     if data["count"] != 0:
         count += data["count"]
         print(f"Count: {data['count']}")
+        make_maesurement_graph(data["results"][0]["id"])
+
         # print(f"Total: {count}")
     # Geeft voor alle ASN's een count van 0, dus geen succes met deze meting
 
-    # Found a great probe here: 753, continuing analysis in seperate file
+    # Found a great probe here: 753 (measurement 23853635), continuing analysis in seperate file
--- Brexit/ping_measurement_analyzer.py
+++ Brexit/ping_measurement_analyzer.py
@@ -3,13 +3,15 @@ from datetime import datetime
 
 # Geen handige manier gevonden om measurements per country te krijgen. Ik ga itereren over de ASN's en deze telkens als target ASN instellen.
 
-data = get_data("https://atlas.ripe.net/api/v2/measurements/23853635/results/")
+def make_maesurement_graph(measurement_id):
+    data = get_data(f"https://atlas.ripe.net/api/v2/measurements/{measurement_id}/results/")
 
-df = pd.json_normalize(data)  # Flattens the data
+    df = pd.json_normalize(data)  # Flattens the data
 
-dates = []
-for date in df['timestamp']:
-    dates.append(str(datetime.fromtimestamp(date)))
+    dates = []
+    for date in df['timestamp']:
+        dates.append(str(datetime.fromtimestamp(date)))
 
-generate_date_plot(df, date_list=dates, data_column_name='avg', plot_title='Average ping', output_file='average_ping.png')
+    generate_date_plot(df, date_list=dates, data_column_name='avg', plot_title='Average ping', output_file=f'average_ping_{measurement_id}.png')
 
+make_maesurement_graph(4103859)
